7bc520647c6c3c89681f409ce4c4653fa667796a
Steven Scaffidi <sscaff1@gmail.com>
2025-06-11 18:13:31 -0500

    chore: changes

--- app/components/Dog/Dog.tsx
+++ app/components/Dog/Dog.tsx
@@ -94,6 +94,14 @@ function Dog({ dog }: DogProps) {
         <div className="dog-section">
           <h2>Description</h2>
           <p className="dog-description">{dog.description}</p>
+          <a
+            className="inline-block mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            href={dog.url}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Learn More on Petfinder
+          </a>
         </div>
 
         {/* Attributes */}
